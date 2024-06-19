206bdfe06a8781076e75403609589fc9f885d2c5
Hazka3 <uryuryu.c@gmail.com>
2024-06-20 03:09:17 +0900

    feat: Add button to navigate to ExplicitAnimationsScreen in MenuScreen

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:std_flutter_animations/screens/explicit_animations_screen.dart';
 import 'package:std_flutter_animations/screens/implicit_animations_screen.dart';
 
 class MenuScreen extends StatelessWidget {
@@ -28,6 +29,13 @@ class MenuScreen extends StatelessWidget {
                 const ImplicitAnimationsScreen(),
               ),
               child: const Text("Implicit Animations"),
+            ),
+            ElevatedButton(
+              onPressed: () => _goToPage(
+                context,
+                const ExplicitAnimationsScreen(),
+              ),
+              child: const Text("Explicit Animations"),
             ),
           ],
         ),
