a7256c943d04499111bcabd5d397e10b2306fed7
Hazka3 <uryuryu.c@gmail.com>
2024-07-19 02:48:32 +0900

    feat: Add 3 color circles on apple watch * - Arc lines have not yet been created.

--- lib/screens/apple_watch_screen.dart
+++ lib/screens/apple_watch_screen.dart
@@ -29,7 +29,51 @@ class _AppleWatchScreenState extends State<AppleWatchScreen> {
 
 class AppleWatchPainter extends CustomPainter {
   @override
-  void paint(Canvas canvas, Size size) {}
+  void paint(Canvas canvas, Size size) {
+    final center = Offset(size.width / 2, size.height / 2);
+
+    final redCircleRadius = (size.width) / 2 * 0.9;
+    final greenCircleRadius = (size.width) / 2 * 0.76;
+    final blueCircleRadius = (size.width) / 2 * 0.62;
+
+    const circleStrokeWidth = 25.0;
+
+    // draw red circle
+    final redCirclePaint = Paint()
+      ..color = Colors.red.shade400.withOpacity(0.3)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = circleStrokeWidth;
+
+    canvas.drawCircle(
+      center,
+      redCircleRadius,
+      redCirclePaint,
+    );
+
+    // draw green circle
+    final greenCirclePaint = Paint()
+      ..color = Colors.green.shade400.withOpacity(0.3)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = circleStrokeWidth;
+
+    canvas.drawCircle(
+      center,
+      greenCircleRadius,
+      greenCirclePaint,
+    );
+
+    // draw blue circle
+    final blueCirclePaint = Paint()
+      ..color = Colors.cyan.shade400.withOpacity(0.3)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = circleStrokeWidth;
+
+    canvas.drawCircle(
+      center,
+      blueCircleRadius,
+      blueCirclePaint,
+    );
+  }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
