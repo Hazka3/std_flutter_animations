8d0a9472b547eb0a56bcd042765f8ef8e17fae73
Hazka3 <uryuryu.c@gmail.com>
2024-07-20 15:08:07 +0900

    feat: Add a card widgets in center of screen

--- lib/screens/swiping_cards_screen.dart
+++ lib/screens/swiping_cards_screen.dart
@@ -14,6 +14,38 @@ class _SwipingCardsScreenState extends State<SwipingCardsScreen> {
       appBar: AppBar(
         title: const Text('Swiping Cards'),
       ),
+      body: const Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Positioned(
+            top: 100,
+            child: Card(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Card extends StatelessWidget {
+  const Card({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Material(
+      elevation: 10,
+      borderRadius: BorderRadius.circular(10),
+      clipBehavior: Clip.hardEdge,
+      child: SizedBox(
+        width: size.width * 0.8,
+        height: size.height * 0.6,
+        child: Image.asset(
+          "assets/covers/1.jpg",
+          fit: BoxFit.cover,
+        ),
+      ),
     );
   }
 }
