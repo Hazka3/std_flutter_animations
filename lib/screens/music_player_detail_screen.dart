d9576abde6c62152b1eb4113d227625e14a3d1ac
Hazka3 <uryuryu.c@gmail.com>
2024-07-30 02:09:53 +0900

    docs: Add music_player_detail_screen.dart file

---
+++ lib/screens/music_player_detail_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MusicPlayerDetailScreen extends StatefulWidget {
+  const MusicPlayerDetailScreen({super.key});
+
+  @override
+  State<MusicPlayerDetailScreen> createState() =>
+      _MusicPlayerDetailScreenState();
+}
+
+class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
