bb3b0d09422ad609757839bb0e20ec3f9028df32
Hazka3 <uryuryu.c@gmail.com>
2024-08-03 13:27:21 +0900

    feat: Implement marquee animation in song information text

--- lib/screens/music_player_detail_screen.dart
+++ lib/screens/music_player_detail_screen.dart
@@ -14,15 +14,28 @@ class MusicPlayerDetailScreen extends StatefulWidget {
 }
 
 class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen>
-    with SingleTickerProviderStateMixin {
+    with TickerProviderStateMixin {
   late final AnimationController _progressController = AnimationController(
     vsync: this,
     duration: const Duration(seconds: 10),
   )..repeat(reverse: true);
 
+  late final AnimationController _marqueeController = AnimationController(
+    vsync: this,
+    duration: const Duration(
+      seconds: 10,
+    ),
+  )..repeat(reverse: true);
+
+  late final Animation<Offset> _marqueeTween = Tween(
+    begin: const Offset(0.1, 0),
+    end: const Offset(-0.6, 0),
+  ).animate(_marqueeController);
+
   @override
   void dispose() {
     _progressController.dispose();
+    _marqueeController.dispose();
     super.dispose();
   }
 
@@ -103,6 +116,31 @@ class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen>
                   ),
                 ),
               ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "Title of the song",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          SlideTransition(
+            position: _marqueeTween,
+            child: const Text(
+              "A Flim By Christopher Nolan - Original Motion Picture Soundtrack",
+              maxLines: 1,
+              overflow: TextOverflow.visible,
+              softWrap: false,
+              style: TextStyle(
+                fontSize: 18,
+              ),
             ),
           ),
         ],
