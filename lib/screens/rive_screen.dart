12c54d1fe030fb98b4ba1cf3e337622c8d856ef5
Hazka3 <uryuryu.c@gmail.com>
2024-08-10 12:35:52 +0900

    docs:Add rive_screen.dart file

---
+++ lib/screens/rive_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class RiveScreen extends StatefulWidget {
+  const RiveScreen({super.key});
+
+  @override
+  State<RiveScreen> createState() => _RiveScreenState();
+}
+
+class _RiveScreenState extends State<RiveScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Rive"),
+      ),
+    );
+  }
+}
