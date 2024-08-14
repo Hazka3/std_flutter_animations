dd7cf848723b418295f5651cc696258a90f0a3a6
Hazka3 <uryuryu.c@gmail.com>
2024-08-15 03:04:19 +0900

    feat: Create a test screen to use the OpenContainer
    The screen has 20 Listtiles.

--- lib/screens/container_transform_screen.dart
+++ lib/screens/container_transform_screen.dart
@@ -15,6 +15,27 @@ class _ContainerTransformScreenState extends State<ContainerTransformScreen> {
       appBar: AppBar(
         title: const Text('Container Transform'),
       ),
+      body: ListView.separated(
+        itemBuilder: (context, index) => ListTile(
+          leading: Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage("assets/covers/${(index % 5) + 1}.jpg"),
+              ),
+            ),
+          ),
+          title: const Text("Dune Soundtrack"),
+          subtitle: const Text("Hans Zimmer"),
+          trailing: const Icon(Icons.arrow_forward_ios),
+        ),
+        separatorBuilder: (context, index) => const SizedBox(
+          height: 20,
+        ),
+        itemCount: 20,
+      ),
     );
   }
 }
