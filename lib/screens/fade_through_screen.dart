8c6e37ed37f72d20c1e5464e4b6e84b058ab3922
Hazka3 <uryuryu.c@gmail.com>
2024-08-17 14:54:14 +0900

    feat: Add button to navigate to FadeThroughScreen in MenuScreen

---
+++ lib/screens/fade_through_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FadeThroughScreen extends StatefulWidget {
+  const FadeThroughScreen({super.key});
+
+  @override
+  State<FadeThroughScreen> createState() => _FadeThroughScreenState();
+}
+
+class _FadeThroughScreenState extends State<FadeThroughScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:std_flutter_animations/screens/apple_watch_screen.dart';
 import 'package:std_flutter_animations/screens/container_transform_screen.dart';
 import 'package:std_flutter_animations/screens/explicit_animations_screen.dart';
+import 'package:std_flutter_animations/screens/fade_through_screen.dart';
 import 'package:std_flutter_animations/screens/implicit_animations_screen.dart';
 import 'package:std_flutter_animations/screens/music_player_screen.dart';
 import 'package:std_flutter_animations/screens/rive_screen.dart';
@@ -84,6 +85,13 @@ class MenuScreen extends StatelessWidget {
                 const SharedAxisScreen(),
               ),
               child: const Text("Shared Axis"),
+            ),
+            ElevatedButton(
+              onPressed: () => _goToPage(
+                context,
+                const FadeThroughScreen(),
+              ),
+              child: const Text("Fade Through"),
             ),
           ],
         ),
