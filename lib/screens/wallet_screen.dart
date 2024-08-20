1a03b10ed73195f1a301753c9fd479e6de12025d
Hazka3 <uryuryu.c@gmail.com>
2024-08-20 20:35:57 +0900

    feat: Add initial animation effects for each card
    - FadeEffect (0 to 1)
    - SlideEffect (x : -1 to 1)

--- lib/screens/wallet_screen.dart
+++ lib/screens/wallet_screen.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 
 class WalletScreen extends StatefulWidget {
@@ -17,14 +15,22 @@ class _WalletScreenState extends State<WalletScreen> {
       appBar: AppBar(
         title: const Text("Wallet"),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(20),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
         child: Column(
           children: [
-            CreditCard(bgColor: Colors.purple),
-            CreditCard(bgColor: Colors.black),
-            CreditCard(bgColor: Colors.blue),
-          ],
+            const CreditCard(bgColor: Colors.purple),
+            const CreditCard(bgColor: Colors.black),
+            const CreditCard(bgColor: Colors.blue),
+          ]
+              .animate(interval: 500.ms)
+              .fadeIn(
+                begin: 0,
+              )
+              .slideX(
+                begin: -1,
+                end: 0,
+              ),
         ),
       ),
     );
